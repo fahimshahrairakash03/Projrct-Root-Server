410701338c039ff1f6528f81014516abd5d4214b
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2024-07-10 17:05:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+//username: project-root-user
+//password:7lq2WYWqp5c9buIh
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4r8e4ne.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("Project-Root").collection("services");
+    const reviewCollection = client.db("Project-Root").collection("reviews");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const serviceId = req.query.serviceid;
+      const emailid = req.query.email;
+      if (serviceId) {
+        const query = { service_id: serviceId };
+        const review = await reviewCollection.find(query).toArray();
+        res.send(review);
+      } else if (emailid) {
+        const query = { email: emailid };
+        const review = await reviewCollection.find(query).toArray();
+        res.send(review);
+      } else {
+        const query = {};
+        const review = await reviewCollection.find(query).toArray();
+        res.send(review);
+      }
+    });
+  } finally {
+  }
+}
+run().catch((e) => console.log(e));
+
+app.get("/", (req, res) => {
+  res.send("App Is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Root Server is Running on port: ${port}`);
+});
